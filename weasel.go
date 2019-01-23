61080c241ec9c6d78b5c1a83273e1e9cd7f8e44d
Sinan Baltacioglu <siphon@gmail.com>
2019-01-22 22:56:17 -0500

    Ok, Go is Cool. Grok'd the basics. TONS of potential applications for projects small and large

--- weasel.go
+++ weasel.go
@@ -105,7 +105,7 @@ func IndexHandler(w http.ResponseWriter, req *http.Request) {
 	status := "weasel index"
 
 	wv := WebView {
-		Title: "Click counter: " + status,
+		Title: status,
 	}
 	// Render page
 	RenderRoute(w, wv)
@@ -117,7 +117,7 @@ func GuideHandler(w http.ResponseWriter, req *http.Request) {
 	status := "weasel guide"
 
 	wv := WebView {
-		Title: "Click counter: " + status,
+		Title: status,
 	}
 	// Render page
 	RenderRoute(w, wv)
@@ -127,13 +127,22 @@ func SearchHandler(w http.ResponseWriter, req *http.Request) {
 	rs := fmt.Sprintf("%#v", req)
 	rs=""
 	fmt.Println("SearchHandler call" + rs)
-	status := "weasel search"
+	status := "do wit.ai: "
+
+	// http://localhost:8080/search?recognition_language=en-US&recognition_lang_tlxd=en-US&weasel_ask=why+should+my+boss+care+about+open+source
+	fmt.Println("GET params were:", req.URL.Query())
+
+	recognitionlanguage := req.FormValue("recognition_language")
+	autotranslate := req.FormValue("recognition_lang_tlxd")
+	searchquery := req.FormValue("weasel_ask")
+
 	vars := mux.Vars(req)
 	q := vars["q"]
-	q = q + " " + status
+	status = status + " " + q + recognitionlanguage + " " + autotranslate + " " + searchquery 
 
+	// todo: next step is to update the webview, match it to the objects we expect
 	wv := WebView {
-		Title: "Click counter: " + status,
+		Title: status,
 	}
 	// Render page
 	//Render(w, "weaseltemplates/gc-ermine.html", wv)
@@ -173,12 +182,34 @@ func weaselCreateRouter(port string) {
 
 	templates, err = template.ParseFiles(allFiles...) //parses all .tmpl files in the 'templates' folder
    
-    
     r.HandleFunc("/", IndexHandler)
-	r.HandleFunc("/guide/", GuideHandler)
-	r.HandleFunc("/search/{q}", SearchHandler)
-	r.HandleFunc("/redirect/{q}", RedirectHandler)	
+	r.HandleFunc("/guide/{q}", GuideHandler)
+	r.HandleFunc("/search", SearchHandler)
+	r.HandleFunc("/api", RedirectHandler)	
 	
+	// Note: This is the moment I realized how cool Go actually is.
+	// It's been fighting me until now to get a basic web page to render.
+	// I just wanted it to get out of my may and do what I expected, but that the thing
+	// Go isn't like the other ones. It's a completely different mindset.
+	// You must be flexible like the branches of a tree lest ye break in the cold canadian winters
+	// 
+	// Also Sin's Cat/Dog Hypothesis applies here: If you pet a cat like you would a dog, you're gonna
+	// get bitten in all probability. If you pet a dog like a cat, well thats an exercise for the reader
+	// most large breeds of dog are heavier than I am. It's still a jungle, no matter how much AI
+	// we endeavor to build
+	//
+	// Enough philosophy, let's get back to the coolness of Go
+	//
+	// so Go is a bit different, your files dont exist unless you say they do.
+	// we're going to invoke magic to create directory that doesnt really exist root/css/
+	// that we can use in our templates (so all css can live together effectively)
+	// same for js. You can use this in a lot more powerful ways. These are just the basics
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/",
+		http.FileServer(http.Dir("weaselstatic/css/"))))
+
+	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/",
+		http.FileServer(http.Dir("weaselstatic/js/"))))
+
 	fmt.Println("Starting webserver on port " + port)
 	http.Handle("/", r)
 
@@ -205,7 +236,6 @@ func main() {
 	}
 	port = "8080"
 
-	weaselCreateRouter(port) 
-	
+	weaselCreateRouter(port) 	
 }
 
